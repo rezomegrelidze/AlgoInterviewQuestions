c8dc34be87287a05f5bfc4541b9d7cd1eb080008
Rezo Megrelidze <rezomegrelidze@gmail.com>
2024-07-25 17:07:18 +0300

    added further comments

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,9 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿SolutionToQuestion5.RunExample();
 
-var people = SolutionToQuestion5.GeneratePeople();
-var five = await SolutionToQuestion5.FindFive(people);
-
-foreach (var person in five)
-{
-    Console.WriteLine($"Id: {person.Id},FirstName: {person.FirstName},LastName: {person.LastName}");
-}
+// Each solution has its own file (SolutionToQuestion[n].cs).
+// not every solution is provided with an example.
